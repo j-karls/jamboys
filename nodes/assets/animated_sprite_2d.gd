e37298f6f46f8f8e85777300d4c5790a9883dc65
j-karls <karlzone0@gmail.com>
2026-06-16 19:43:34 +0200

    Added gay color

--- nodes/assets/animated_sprite_2d.gd
+++ nodes/assets/animated_sprite_2d.gd
@@ -11,3 +11,23 @@ func _process(_delta: float) -> void:
 		self.play("walk")
 	else:
 		self.stop()
+
+# This function is triggered whenever the StatsManager emits its signal
+func _on_stats_manager_buffs_applied(active_buffs: Array[Buff]) -> void:
+	var should_color: bool = false
+	
+	for buff in active_buffs:
+		if buff.character_effect == "colorful":
+			should_color = true
+			break
+			
+	if should_color:
+		_apply_color()
+	else:
+		_reset_color()
+
+func _apply_color() -> void:
+	self.modulate = Color(0.91, 0.227, 1.0, 1.0)
+
+func _reset_color() -> void:
+	self.modulate = Color(1.0, 1.0, 1.0, 1.0)
